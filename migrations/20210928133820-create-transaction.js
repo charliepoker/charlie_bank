461104cfcb3fd41c46de8515755a34abcc289441
charliepoker <iheanachocharlie@gmail.com>
2021-09-28 14:58:05 +0100

    transaction migration was generated

---
+++ migrations/20210928133820-create-transaction.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      transactionId: {
+        type: Sequelize.INTEGER
+      },
+      transactionAmount: {
+        type: Sequelize.FLOAT
+      },
+      from_wallet: {
+        type: Sequelize.INTEGER
+      },
+      to_wallet: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  }
+};
